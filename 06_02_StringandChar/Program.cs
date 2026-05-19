8f89cb88e71aec7d699926fa8979a23e846036d8
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 17:44:13 +0300

    string ve char ilişkisi eklendi

---
+++ 06_02_StringandChar/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _06_02_StringandChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i = 65; i < 90; i++)
+            {
+                char lettercode = (char)i;
+                Console.WriteLine(lettercode+ " ");
+            }
+           Console.ReadKey();
+        }
+    }
+}
