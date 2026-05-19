97619b1e024e5cc588646309160517548738848d
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 16:52:28 +0300

    Add project files.

---
+++ 06_01_StringVeritipi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06_01_StringVeritipi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
