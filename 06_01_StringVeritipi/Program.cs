bee7cdf9c9306ab990e79b53f41bd5a50df0d1c3
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 17:27:30 +0300

    string metotlari bitti

--- 06_01_StringVeritipi/Program.cs
+++ 06_01_StringVeritipi/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string message = "merhaba";
+            Console.WriteLine("ilk mesaj:"+message);
+            string newMessage = message + " dünya";
+            Console.WriteLine("ilk mesaj:" + message);
+            Console.WriteLine("yeni mesaj:" + message);
+            Console.WriteLine("mesajın ilk karakteri:"+message[0]);
+            Console.WriteLine("mesajın son karakteri: " + message[message.Length - 1]);
+            Console.WriteLine("mesajın karakterleri");
+            foreach (char c in message) 
+            { Console.WriteLine(c);
+            }
+            Console.WriteLine($"Büyük harfe çevirme:{message.ToUpper()} ");
+            Console.WriteLine($"içinde dünya varmı:{message.Contains("Dünya")} "); ;
+            Console.ReadKey();
         }
     }
 }
