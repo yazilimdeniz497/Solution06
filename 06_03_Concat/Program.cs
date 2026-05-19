312b541c51676b82d9b449f87551f0af25e33664
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 18:11:38 +0300

    concat ve compareto eeklendi

---
+++ 06_03_Concat/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Adınızı Giriniz: ");
+string firstname=Console.ReadLine();
+Console.WriteLine("Soyadinizi Giriniz: ");
+string lastname=Console.ReadLine();
+string welcome1 = "Merhaba" + firstname + " " + lastname + ".";
+
+string welcome2 = String.Concat("Sayin", firstname, lastname);
+Console.WriteLine(welcome1);
+Console.WriteLine(welcome2);
+Console.ReadKey();
