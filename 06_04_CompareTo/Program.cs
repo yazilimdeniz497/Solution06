2f714fe1533a4316cb4ca219d8c793d46e71a150
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-20 08:28:43 +0300

    solution06 bitti

--- 06_04_CompareTo/Program.cs
+++ 06_04_CompareTo/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var a = "Istanbul";
+var b = "istanbul";
+Console.WriteLine($"{a}=={b}?{a==b}");
+Console.ReadKey();
---
+++ 06_05_StringBuilder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+String Email = "zfercomert@gmail.com";
+String[] endpoints = { "/home", "/products", "/usb-cable" };
+string report1 = BuildReport(Email, endpoints, sessionId: "S-1001");
+Console.WriteLine(report1);
+string BuildReport(string email, string[] endpoints, string sessionId)
+{
+    var sb = new StringBuilder(capacity:256);
+    sb.AppendLine($"Email:{email}");
+    sb.AppendLine($"Session:{sessionId}");
+    sb.AppendLine("Visited: ");
+    foreach(var item in endpoints)
+    {
+        sb.AppendLine(item);
+    }
+    string report = sb.ToString();
+    return report;
+
+}
+Console.ReadKey();
